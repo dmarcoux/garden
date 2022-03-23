fc5dbf276f7551a1c2919c02b87216aa62c4c482
Dany Marcoux <dmarcoux@posteo.de>
2022-03-23 12:43:42 +0100

    Translate website and page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def website_title
+    "#{I18n.t(:website_title)} - #{page_title}"
+  end
+
+  def page_title
+    # Pass a custom page title in a view with:
+    # <%= content_for(:page_title, I18n.t(".page_title", some_interpolation: @something.name)) %>
+    # Otherwise, look up the translation based on the controller path and action name
+    @page_title ||= content_for(:page_title).presence || I18n.t("#{controller_path.tr('/', '.')}.#{action_name}.page_title")
+  end
 end
