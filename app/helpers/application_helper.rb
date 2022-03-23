bc24b8bbe29f585c87000019a1047502848bdbe8
Dany Marcoux <dmarcoux@posteo.de>
2022-03-23 18:25:01 +0100

    Fix RuboCop offense in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,7 @@ module ApplicationHelper
     # Pass a custom page title in a view with:
     # <%= content_for(:page_title, I18n.t(".page_title", some_interpolation: @something.name)) %>
     # Otherwise, look up the translation based on the controller path and action name
-    @page_title ||= content_for(:page_title).presence || I18n.t("#{controller_path.tr('/', '.')}.#{action_name}.page_title")
+    @page_title ||= content_for(:page_title).presence ||
+                    I18n.t("#{controller_path.tr('/', '.')}.#{action_name}.page_title")
   end
 end
